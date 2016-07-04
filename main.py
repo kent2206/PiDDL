a09a080b5ab008ab877c1bb2a3557d1c8878d680
kent2206 <kent.collonges@hotmail.fr>
2016-07-04 17:38:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@
 #Pyload_Client('localhost', '8000', 'julien', 'gTV7y5Te').getServerVersion()
 from ZTPAGE import *
 page = ZTPage('http://www.zone-telechargement.com/series/vostfr/104377-billy-billie-saison-1.html')
-# page.printReport()
+# page.print_report()
 # page.getTVShowLink('Uploaded',1)
